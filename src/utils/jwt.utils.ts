708e23850c3891d7872d7c8e16e289476f4425f1
ansarworkingspace <ansarworkingspace@gmail.com>
2025-06-24 15:57:08 +0530

    dev

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -35,6 +35,7 @@ export const setTokenCookie = (res: Response, token: string): void => {
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
+    domain: "https://ecomzfrontend.vercel.app",
   });
 };
 
@@ -45,5 +46,6 @@ export const clearTokenCookie = (res: Response): void => {
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
+    domain: "https://ecomzfrontend.vercel.app",
   });
 };
